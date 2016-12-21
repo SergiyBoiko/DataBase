42830a02b752c076bf95c250b4903351f4202709
SergiyBoiko <codedva00@gmail.com>
2016-12-21 19:23:16 +0200

    add method getConnection

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
+
+    private static Connection getConnection()throws SQLException,ClassNotFoundException {
+    Class.forName("org.h2.Driver");
+    String url = "jdbc:h2:~/myDataBase;IFEXISTS=TRUE";
+    return DriverManager.getConnection(url, "sa", "");
+}
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         WorkWithDataBase db = new WorkWithDataBase();
-        db.createDataBase();
-        db.addDataInDataBase();
-        db.showDataInDataBase();
-        db.totalSumSalary();
+        Connection connection = getConnection();
+        db.createDataBase(connection);
+        db.addDataInDataBase(connection);
+        db.showDataInDataBase(connection);
+        db.totalSumSalary(connection);
     }
+
+
 }
